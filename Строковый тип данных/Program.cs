dc0cae7cc3b616e4e63830402da104aeb7046d34
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-11-30 15:41:21 +0200

    Добавьте файлы проекта.

---
+++ Строковый тип данных/Program.cs
@@ -0,0 +1,21 @@
+﻿public class FindDuplicateWords
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение (слова через пробел):");
+        string sentence = Console.ReadLine().ToLower();
+
+        string[] words = sentence.Split(' ');
+
+        var kolichestvoslov = words.GroupBy(x => x).Where(g => g.Count() == 2).Select(g => g.Key);
+
+        if (kolichestvoslov.Count() > 0)
+        {
+            Console.WriteLine("Одинаковые слова: " + kolichestvoslov.First());
+        }
+        else
+        {
+            Console.WriteLine("Одинаковых слов не найдено или их больше двух.");
+        }
+    }
+}
